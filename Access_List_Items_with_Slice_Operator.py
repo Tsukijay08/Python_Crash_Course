34f3389b802cdd1f39e4493d4b94ba55444ddc3e
Tsukijay08 <delacruz.jojitjr.d.1458@gmail.com>
2025-07-16 13:38:21 +0800

    String and list lesson

---
+++ Access_List_Items_with_Slice_Operator.py
@@ -0,0 +1,7 @@
+list1 = ["a", "b", "c", "d"]
+list2 = [25.50, True, -55, 1+2j]
+list3 = ["Rohan", "Physics", 21, 69.75]
+
+print ("Items from index 1 to last in list1: ", list1[1:])
+print ("Items from index 0 to 1 in list2: ", list2[:2])
+print ("Items from index 0 to index last in list3", list3[:])
